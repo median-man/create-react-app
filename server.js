c7730cb69f978cb199088589585acf138c38502d
John Desrosiers <johndrunner@yahoo.com>
2017-10-17 12:58:04 -0700

    fix linting errors

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
-app.get("/healthcheck", function(req,res){
-  res.json({"success": true, status: 200})
+app.get('/healthcheck', (req, res) => {
+  res.json({ success: true, status: 200 });
 });
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
