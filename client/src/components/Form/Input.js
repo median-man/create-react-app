367fc87905911159f31b46b1a361b1be43740f2f
John Desrosiers <johndrunner@yahoo.com>
2017-10-18 07:37:00 -0700

    resolved conflict in Main.js -- white space only

--- client/src/components/Form/Input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Input = ({ id, label, ...props }) => (
-  <div className="form-group">
-    <label htmlFor={id}>{label}</label>
-    <input className="form-control" id={id} {...props} />
-  </div>
-);
-
