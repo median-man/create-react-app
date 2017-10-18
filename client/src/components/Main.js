4ab2a80c1b2fa446933079d684f2144e7012c88e
John Desrosiers <johndrunner@yahoo.com>
2017-10-18 08:21:51 -0700

    Main component written as a class

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Jumbotron from './Jumbotron';
 import { Col, Container, Row } from './Grid';
 import { FormBtn, Input } from './Form';
 
-const Main = ({ children }) => (
-  <div>
-    <Container>
-      {/* Page Header */}
-      <Jumbotron>
-        <h1 className="text-center">
-          <strong><i className="fa fa-newspaper-o" /> New York Times Search</strong>
-        </h1>
-      </Jumbotron>
-
-      <form>
-        <Input id="topic" label="Topic:" name="topic" type="text" required />
-        <Input id="startYear" label="Start Year:" name="topic" type="number" />
-        <Input id="endYear" label="End Year:" name="topic" type="number" />
-        <FormBtn id="btnSearch" type="submit">Search</FormBtn>
-      </form>
-
-      { children }
-
-      {/* Footer Row */}
-      <Row>
-        <Col size="xs-12">
-          <hr />
-          <h5 className="text-center"><small>Made by John Desrosiers.</small></h5>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          {/* Page Header */}
+          <Jumbotron>
+            <h1 className="text-center">
+              <strong><i className="fa fa-newspaper-o" /> New York Times Search</strong>
+            </h1>
+          </Jumbotron>
+    
+          <form>
+            <Input id="topic" label="Topic:" name="topic" type="text" required />
+            <Input id="startYear" label="Start Year:" name="topic" type="number" />
+            <Input id="endYear" label="End Year:" name="topic" type="number" />
+            <FormBtn id="btnSearch" type="submit">Search</FormBtn>
+          </form>
+    
+          { this.props.children }
+    
+          {/* Footer Row */}
+          <Row>
+            <Col size="xs-12">
+              <hr />
+              <h5 className="text-center"><small>Made by John Desrosiers.</small></h5>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default Main;
