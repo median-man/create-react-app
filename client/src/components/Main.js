40f542a2970067540ef779e394ac5c2b1430fb67
John Desrosiers <johndrunner@yahoo.com>
2017-10-18 06:25:44 -0700

    removed nav component

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import Jumbotron from './Jumbotron';
 import { Col, Container, Row } from './Grid';
-import Nav from './Nav';
-
 const Main = ({ children }) => (
-  <Container>
-    <Nav />
-    {/* Page Header */}
-    <Jumbotron>
-      <h1 className="text-center">
-        <strong><i className="fa fa-newspaper-o" /> New York Times Search</strong>
-      </h1>
-    </Jumbotron>
-    { children }
+  <div>
+    <Container>
+      {/* Page Header */}
+      <Jumbotron>
+        <h1 className="text-center">
+          <strong><i className="fa fa-newspaper-o" /> New York Times Search</strong>
+        </h1>
+      </Jumbotron>
+      { children }
 
-    {/* Footer Row */}
-    <Row>
-      <Col size="xs-12">
-        <hr />
-        <h5 className="text-center"><small>Made by John Desrosiers.</small></h5>
-      </Col>
-    </Row>
-  </Container>
+      {/* Footer Row */}
+      <Row>
+        <Col size="xs-12">
+          <hr />
+          <h5 className="text-center"><small>Made by John Desrosiers.</small></h5>
+        </Col>
+      </Row>
+    </Container>
+  </div>
 );
 
 export default Main;
--- client/src/components/Nav/Nav.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-const Nav = ({ children }) => (
-  <div>TODO Nav Bar</div>
-);
-
-export default Nav;
