5a416f6c4325933494677b4fc7d27c026c03f4eb
John Desrosiers <johndrunner@yahoo.com>
2017-10-17 17:44:05 -0700

    added express and configuration and db connection

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const articles = require('./controllers/articles.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Serve up static assets (usually on heroku)
+// configure express
+// ==========================================================
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+app.use(bodyParser.json());
 
+// server routes
+// ==========================================================
+
+
+// TODO remove healthceck route
 app.get('/healthcheck', (req, res) => {
   res.json({ success: true, status: 200 });
 });
@@ -18,6 +28,12 @@ app.get('/healthcheck', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
+
+// Connect to database
+// ==========================================================
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/nytSearch';
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGODB_URI, { useMongoClient: true });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
