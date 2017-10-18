1c3ca69e0a110b8eb92ae4650631acf705f55651
John Desrosiers <johndrunner@yahoo.com>
2017-10-18 05:48:02 -0700

    refactor to implement Grid components

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Jumbotron from './Jumbotron';
-import Footer from './Footer';
+import { Col, Container, Row } from './Grid';
 
 const Main = ({ children }) => (
-  <div className="container">
+  <Container>
+    {/* Page Header */}
     <Jumbotron>
       <h1 className="text-center">
         <strong><i className="fa fa-newspaper-o" /> New York Times Search</strong>
       </h1>
     </Jumbotron>
     { children }
-    <Footer />
-  </div>
+
+    {/* Footer Row */}
+    <Row>
+      <Col size="xs-12">
+        <hr />
+        <h5 className="text-center"><small>Made by John Desrosiers.</small></h5>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default Main;
