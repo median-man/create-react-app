02565387e0260e0e32ffdfd2f5971bc168954b7e
John Desrosiers <johndrunner@yahoo.com>
2017-10-19 05:16:27 -0700

    convert App component to pure function

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Search from './components/Search';
 import Saved from './components/Saved';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-          <Switch>
-            <Route exact path="/" component={Search} />
-            <Route exact path="/search" component={Search} />
-            <Route exact path="/saved" component={Saved} />
-          </Switch>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Search} />
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/saved" component={Saved} />
+    </Switch>
+  </Router>
+);
 export default App;
