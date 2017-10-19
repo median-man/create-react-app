780b039b534758294ea4460b635f6c3fedd620b8
John Desrosiers <johndrunner@yahoo.com>
2017-10-19 04:27:32 -0700

    initial search page and form

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Main from './Main';
+import { FormBtn, Input } from './Form';
+import searchNYT from '../utils/timesAPI';
 
-const Search = () => (
-  <div></div>
-);
+class Search extends Component {
+    state = {
+      articles: []
+    };
 
+    // handler for change in form inputs
+    handleInputChange = event => {
+      const { name, value } = event.target;
+      this.setState({ [name]: value });
+    };
+
+    // search for articles from nyt
+    submitSearch = event => {
+      event.preventDefault();
+      searchNYT( this.state.topic )
+      // TODO Finish handling data from nyt api
+        .then(data => {
+          this.setState({ articles: data });
+        });
+    }
+  render() {
+    return (
+        <Main>
+          <form>
+            <Input 
+              id="topic" 
+              label="Topic:" 
+              name="topic" 
+              type="text" 
+              value={this.state.searchTopic}
+              required
+              onChange={this.handleInputChange}
+            />
+            <Input
+              id="startYear"
+              label="Start Year:"
+              name="topic"
+              type="number"
+              onChange={this.handleInputChange}
+            />
+            <Input
+              id="endYear"
+              label="End Year:"
+              name="topic"
+              type="number"
+              onChange={this.handleInputChange}
+            />
+            <FormBtn
+              id="btnSearch"
+              type="submit"
+              disabled={!this.state.topic}
+              onClick={this.submitSearch}
+            >
+              Search
+            </FormBtn>
+          </form>
+        </Main>
+
+    );
+}
+}
 export default Search;
