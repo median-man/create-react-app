c2a53486b6dc53a332fde22d1d29615c4d2995b7
John Desrosiers <johndrunner@yahoo.com>
2017-10-19 05:44:57 -0700

    changed styling of jumbotron

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-// css styles applied to jumbotron div
-const jtstyles = {
-  backgroundColor: '#20315A',
-  color: 'white',
-};
-
 const Jumbotron = ({ children }) => (
-  <div style={jtstyles} className="jumbotron">
+  <div className="jumbotron">
     {children}
   </div>
 );
