d7412d1da1840706347b078a5c55ab82d35196dc
John Desrosiers <johndrunner@yahoo.com>
2017-10-19 04:21:09 -0700

    changed main to pure function

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,32 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Jumbotron from './Jumbotron';
 import { Col, Container, Row } from './Grid';
 
-class Main extends Component {
-  render() {
-    return (
-      <div>
-        <Container>
-          {/* Page Header */}
-          <Jumbotron>
-            <h1 className="text-center">
-              <strong><i className="fa fa-newspaper-o" /> New York Times Search</strong>
-            </h1>
-          </Jumbotron>
-    
-          { this.props.children }
-    
-          {/* Footer Row */}
-          <Row>
-            <Col size="xs-12">
-              <hr />
-              <h5 className="text-center"><small>Made by John Desrosiers.</small></h5>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-}
+
+const Main = props => (
+  <div>
+    <Container>
+      {/* Page Header */}
+      <Jumbotron>
+        <h1 className="text-center">
+          <strong><i className="fa fa-newspaper-o" /> New York Times Search</strong>
+        </h1>
+      </Jumbotron>
+
+      {props.children}
+
+      {/* Footer Row */}
+      <Row>
+        <Col size="xs-12">
+          <hr />
+          <h5 className="text-center"><small>Made by John Desrosiers.</small></h5>
+        </Col>
+      </Row>
+    </Container>
+  </div>
+);
 
 export default Main;
