f44bb6b912c4d0a2ca72c4882dcf98eeb975b1ff
John Desrosiers <johndrunner@yahoo.com>
2017-10-19 19:46:04 -0700

    begin developing Saved page component

--- client/src/components/pages/Saved.js
+++ client/src/components/pages/Saved.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Main from '../Main';
+import Articles from '../Articles';
 
 class Saved extends Component {
   render() {
+    let articleView = <div>No saved articles.</div> ;
+    if (this.props.articles.length > 0) {
+      articleView = <Articles articles={this.props.articles} />;
+    }
     return (
       <Main>
-        Saved Articles
+        <h2>Saved Articles</h2>
+        <hr />
+        {articleView}
       </Main>
     );
   }
