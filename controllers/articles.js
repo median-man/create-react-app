c5f07004aee990bc67eadc27b806ff0aa943d5b3
John Desrosiers <johndrunner@yahoo.com>
2017-10-19 20:01:36 -0700

    GET api/saved route now serves saved articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -5,7 +5,9 @@ const Article = require('../models/Article');
 module.exports = {
   // Returns a promise for all saved articles
   all: (req, res) => {
-    // TODO save article
+    return Article.find()
+      .then(result => res.json(result))
+      .catch(err => res.status(500).json(err));
   },
 
   // Adds article to the database and sends response. Returns promise.
@@ -13,7 +15,7 @@ module.exports = {
     // save title, url, and date properties from request body
     return Article.create(req.body)
       .then(articleModel => res.json(articleModel))
-      .catch(error => res.status(422).json(error));
+      .catch(error => res.status(400).json(error));
   },
 
   // Returns a promise to delete an article
